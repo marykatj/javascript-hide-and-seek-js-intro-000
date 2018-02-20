f561f424c887aac1f32713084717446737b4f95b
Mary-Kate James <marykatj@gmail.com>
2018-02-20 15:44:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function getFirstSelector(selector) {
-  document.querySelector(selector)
+  var element = document.querySelector(selector);
+  return element;
+}
+
+function nestedTarget() {
+  var element = document.querySelector('div#nested div.target');
+  return element;
+}
+function increaseRankBy(n) {
+  var element = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < element.length; i++) {
+    element[i].innerHTML = parseInt(i+n).toString()
+  }
+  return element;
 }
